bc35ea48aed6a8e424f9e93b47f872809da70a7e
Derfarctor <97409490+derfarctor@users.noreply.github.com>
2022-04-11 21:57:29 +0100

    Many wallet features added. Refined messaging.

---
+++ src/src/defaults.rs
@@ -0,0 +1,2 @@
+pub const DEFAULT_REP_BANANO: &str = "ban_3catgir1p6b1edo5trp7fdb8gsxx4y5ffshbphj73zzy5hu678rsry7srh8b";
+pub const DEFAULT_REP_NANO: &str = "nano_3zx7rus19yr5qi5zmkawnzo5ehxr7i73xqghhondhfrzftgstgk4gxbubwfq";
