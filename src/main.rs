20739519d5ea393b8b30d2bbc808ae8f207d395e
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2022-04-12 08:16:38 -0700

    Gets rid of trailing whitespace in seed (#1)
    * Gets rid of trailing whitespace in seed
    
    Before, if there was trailing whitespace, the character check would fail due to not being 64 characters long.
    
    * modified to suggestion

--- src/main.rs
+++ src/main.rs
@@ -767,14 +767,15 @@ fn get_seed(s: &mut Cursive) {
             )
             .h_align(HAlign::Center)
             .button("Done", |s| {
-                let seed = s
+                let raw_seed = s
                     .call_on_name("seed", |view: &mut EditView| view.get_content())
                     .unwrap();
+                let seed = raw_seed.trim();
                 if seed.len() != 64 {
                     s.add_layer(Dialog::info("Seed was invalid: not 64 characters long."));
                     return;
                 }
-                let bytes_opt = hex::decode(&*seed);
+                let bytes_opt = hex::decode(seed);
                 if bytes_opt.is_err() {
                     s.add_layer(Dialog::info("Seed was invalid: failed to decode hex."));
                     return;
